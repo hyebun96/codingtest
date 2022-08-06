dffe4568c4dc5a7ff6c1fa601bbf94ec0b6492f9
hyebun <hyebun96@naver.com>
2022-08-06 21:50:44 +0900

    #1 -  Hash Table 구현 - chaining 기법
    HashTable 구현 
    - 데이터 저장
    - 데이터 가져오기
    - 데이터 저장할 때 키 값 간단히 계산할 수 있도록 function 추가
    - 기존 hashtable의 단점을 보안한 chaing 기법 (키 값 충돌시, 링크드 리스트로 추가 할 수있게 구현)

---
+++ CodingTest/MyHashTable_Chaning.java
@@ -0,0 +1,73 @@
+package CodingTest;
+
+public class MyHashTable_Chaning {
+    public Slot[] hashTable;
+
+    public MyHashTable_Chaning(Integer size){
+        this.hashTable = new Slot[size];
+    }
+
+    public class Slot{
+        String key;
+        String value;
+        Slot next;
+        Slot (String key, String value){
+            this.key = key;
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    public boolean saveData(String key, String value){
+        int address = hashFunc(key);
+        if(this.hashTable[address] != null){
+            Slot findSlot = this.hashTable[address];
+            Slot prevSlot = this.hashTable[address];
+
+            while(findSlot != null){
+               if(findSlot.equals(key)){
+                   findSlot.value = value;
+                   return true;
+               } else {
+                   prevSlot = findSlot;
+                   findSlot = findSlot.next;
+               }
+            }
+            prevSlot.next = new Slot(key, value);
+        } else {
+            this.hashTable[address] = new Slot(key, value);
+        }
+        return true;
+    }
+
+    public String getData(String key){
+        int address = this.hashFunc(key);
+        if(this.hashTable[address] != null){
+            Slot findSlot = this.hashTable[address];
+            while(findSlot != null){
+                if(findSlot.key.equals(key)){
+                    return findSlot.value;
+                } else {
+                    findSlot = findSlot.next;
+                }
+            }
+            return null;
+        } else {
+            return null;
+        }
+    }
+
+    public int hashFunc(String key){
+        return (int)key.charAt(0) %  hashTable.length;
+    }
+
+    public static void main(String[] args) {
+        MyHashTable_Chaning mht = new MyHashTable_Chaning(20);
+
+        mht.saveData("DaveLee", "01022223333");
+        mht.saveData("fun-coding", "01033334444");
+        mht.saveData("David", "01044445555");
+        mht.saveData("Dave", "01055556666");
+        System.out.println(mht.getData("DaveLee"));
+    }
+}
