413d79159bb6a6b9bb9b548861c35ec7f0fb7c4e
hyebun <hyebun96@naver.com>
2022-08-15 21:55:00 +0900

    #2 - 탐욕알고리즘2
    문제 풀기 전 1차원 배열이 아닌 객체를 정렬하기 위해
    Comparable 인터페이스에서 compareTo를 오버라이드해 정렬 기준을 재정의 해봄

---
+++ Algorithm/Edge.java
@@ -0,0 +1,31 @@
+package Algorithm;
+
+import java.util.Arrays;
+
+public class Edge implements Comparable<Edge>{
+    public Integer distance;
+    public String vertex;
+
+    public Edge(Integer distance, String vertex) {
+        this.distance = distance;
+        this.vertex = vertex;
+    }
+
+    @Override
+    public int compareTo(Edge e) {
+        return this.distance - e.distance;
+    }
+
+    public static void main(String[] args) {
+        Edge edge1 = new Edge(12, "A");
+        Edge edge2 = new Edge(10, "A");
+        Edge edge3 = new Edge(13, "A");
+
+        Edge[] edges = new Edge[]{edge1, edge2, edge3};
+        Arrays.sort(edges);
+        for (Edge edge : edges) {
+            System.out.println(edge.distance);
+        }
+    }
+}
+
